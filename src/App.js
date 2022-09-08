877b197944c564d305f015750fc3eb5ee3d1be75
lashaKLG <lasha2772@gmail.com>
2022-09-08 18:49:53 +0200

    live-search

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const[loading, setLoading] = useState(false);
+  const [posts, setPosts] =useState([]);
+  const [searchTitle, setSearchTitle] =useState('');
+
+
+  useEffect(() => {
+    const loadPosts = async () => {
+      setLoading(true);
+      const responce = await axios.get('http://jsonplaceholder.typicode.com/posts');
+      setPosts(responce.data);
+      setLoading(false);
+    }
+
+    loadPosts();
+  },[]); 
+
+  return (
+    <div className="App">
+      <h3>Search Filter</h3>
+      <input className='Search'
+        type='text'
+        placeholder='Search...'
+        onChange={(e) => setSearchTitle(e.target.value)}
+      />
+      {loading ? (<h4>Loading ...</h4>): (
+        (posts.filter((value) => {
+          if(searchTitle === '') {
+            return value
+          }else if (value.title.toLowerCase().includes(searchTitle.toLowerCase())) {
+            return value
+          }
+        })
+          .map(item => <h5 key={item.id}>{item.title}</h5>))
+      )}
+    </div>
+  );
+}
+
+export default App;
